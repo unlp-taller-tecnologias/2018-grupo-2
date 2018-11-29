bf76bf4d7b907ad68aa3720368018077365d9472
matiascondem <matiascondemartinez1997@gmail.com>
2018-11-29 09:51:58 -0300

    fixtures mejorados. solo falta el de fauna porque tenemos que reacomodar la parte del destino

--- src/AppBundle/DataFixtures/AppFixtures.php
+++ src/AppBundle/DataFixtures/AppFixtures.php
@@ -15,6 +15,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+
     public function load(ObjectManager $manager)
     {
         // crea 20 areas
@@ -23,6 +24,7 @@ class AppFixtures extends Fixture
             $area->setName('Area '.$i);
             $area->setPolygon(mt_rand(10, 100));
             $manager->persist($area);
+            $this->addReference('area'.$i, $area);
         }
 
         //crea 10 categorias
@@ -30,6 +32,7 @@ class AppFixtures extends Fixture
             $category=new Category();
             $category->setName('Categoria '.$i);
             $manager->persist($category);
+            $this->addReference('category'.$i, $category);
         }
 
         //crea 20 encargados sin linkear con sus categorias
@@ -38,8 +41,56 @@ class AppFixtures extends Fixture
             $attendant->setEmail($this->generateRandomString(10).'@gmail.com');
             $attendant->setName($this->generateRandomString(8));
             $attendant->setLastName($this->generateRandomString(8));
+            $attendant->setCategory($this->getReference('category'.rand(0, 9)));
             $manager->persist($attendant);
         }
+
+        //crea 20 especies de fauna
+        for ($i = 0; $i < 20; $i++) {
+            $specie_fauna=new FASpecie();
+            $specie_fauna->setName('EspecieFauna '.$i);
+            $manager->persist($specie_fauna);
+            $this->addReference('faspecie'.$i, $specie_fauna);
+        }
+
+        //crea 10 subspecies de fauna
+        for ($i = 0; $i < 10; $i++) {
+            $subspecie_fauna=new FASubspecie();
+            $subspecie_fauna->setName('SubespecieFauna '.$i);
+            $subspecie_fauna->setSpecie($this->getReference('faspecie'.rand(0,19)));
+            $manager->persist($subspecie_fauna);
+        }
+
+        //crea 20 especies de flora
+        for ($i = 0; $i < 20; $i++) {
+            $specie_flora=new FLSpecie();
+            $specie_flora->setName('EspecieFlora '.$i);
+            $manager->persist($specie_flora);
+            $this->addReference('flspecie'.$i, $specie_flora);
+        }
+
+        //crea 10 subspecies de flora
+        for ($i = 0; $i < 10; $i++) {
+            $subspecie_flora=new FLSubspecie();
+            $subspecie_flora->setName('SubespecieFlora '.$i);
+            $subspecie_flora->setSpecie($this->getReference('flspecie'.rand(0,19)));
+            $manager->persist($subspecie_flora);
+            $this->addReference('flsubspecie'.$i, $subspecie_flora);
+        }
+
+        //crea 50 individuos de flora
+        for ($i = 0; $i < 50; $i++) {
+            $flora=new Flora();
+            $flora->setObservations($this->generateRandomString(15));
+            #CAMBIAR ESTO DE EDAD AL ALTERAR LA BD
+            $flora->setAge(rand(0,100));
+            $flora->setName('plantita '.$i);
+            $flora->setArea($this->getReference('area'.rand(0, 19)));
+            $flora->setSubspecie($this->getReference('flsubspecie'.rand(0,9)));
+            $manager->persist($flora);
+        }
+
+
 
 
 
